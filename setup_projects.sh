982bb1fad11028d471f624d4976a7f34619747a9
suprabhatrijal <goodguysman@gmail.com>
2022-02-10 17:43:17 +0545

    MVP complete

--- setup_projects.sh
+++ setup_projects.sh
@@ -11,27 +11,41 @@ do
     esac
 done
 
+
 if [[ $mode = "create" ]]; then
 	if [[ $project = "python" ]]; then
 		path="/home/suppe/programming/PycharmProjects"
-		mkdir "$path/$1"
-		cd "$path/$1"
+		mkdir "$path/$2"
+		cd "$path/$2"
 		python3 -m virtualenv venv 
 	elif [[ $project = "flutter" ]]; then
 		path="/home/suppe/programming/FlutterProjects"
 		cd "$path"
-		flutter create $1
-		cd "$path/$1"
+		flutter create $2
+		cd "$path/$2"
 	fi
 
 	touch README.md
-	python3 /home/suppe/programming/PycharmProjects/automate_projects/create_new_repo.py $project $1
+	python3 /home/suppe/programming/PycharmProjects/automate_projects/create_new_repo.py $project $2
 	git init
-	git remote add origin git@github.com:suprabhatrijal/$3.git
+	git remote add origin git@github.com:suprabhatrijal/$2.git
 	git add -A
 	git commit -m "Initial Commit"
 	git pull origin main --rebase
 	git push origin main
+
+elif [[ $mode = "list" && $project = "python" ]]; then
+		cd "/home/suppe/programming/PycharmProjects"
+		echo "Python Projects:" 
+		echo "============================================================================================================================" 
+		ls -1
+		echo "============================================================================================================================" 
+elif [[ $mode = "list" && $project = "flutter" ]]; then
+		cd "/home/suppe/programming/FlutterProjects"
+		echo "Flutter Projects:" 
+		echo "============================================================================================================================" 
+		ls -1
+		echo "============================================================================================================================" 
 elif [[ $mode = "list" ]]; then
 		cd "/home/suppe/programming/PycharmProjects"
 		echo "Python Projects:" 
@@ -42,4 +56,12 @@ elif [[ $mode = "list" ]]; then
 		echo "Flutter Projects:" 
 		echo "============================================================================================================================" 
 		ls -1	
+		echo "============================================================================================================================" 
+		
+elif [[ $mode = "open" && $project = "python" ]]; then
+		cd "/home/suppe/programming/PycharmProjects/$2"
+		vim
+elif [[ $mode = "open" && $project = "flutter" ]]; then
+		cd "/home/suppe/programming/FlutterProjects/$2"
+		vim
 fi
